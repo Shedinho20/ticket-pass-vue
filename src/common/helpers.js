0f713b67d66b16843731bd10c576c4783bd6f662
shedinho20 <shedinhoshedrach@gmail.com>
2022-06-14 08:05:28 +0100

    minor change

--- src/common/helpers.js
+++ src/common/helpers.js
@@ -2,8 +2,6 @@ export const getDate = time => {
   const _date = time * 1000;
 
   const date = new Date(_date).toDateString();
-  const hour = new Date(_date).getUTCHours();
-  const min = new Date(_date).getUTCMinutes();
 
-  return `${date} ${hour}:${min} UTC`;
+  return `${date} `;
 };
